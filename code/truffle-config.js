0074abf4eccbdeb601c74c2bc114fa2ab7911728
sumitkute <sumit_kute@yahoo.com>
2019-11-16 21:27:32 +0530

    Deleted not required files

--- code/truffle-config.js
+++ code/truffle-config.js
@@ -3,17 +3,10 @@ const fs = require('fs');
 module.exports = {
   networks: {
     development: {
-      host: "https://rfmanufacturer.blockchain.azure.com:3200/cbkb8TY2i3TIO-NyHMiB-k6l",
-      port: 3200,
+      host: "127.0.0.1",
+      port: 8545,
       network_id: "*",
-      type: "quorum"
-    },
-    supplychainconsort: {
-      network_id: "*",
-      gas: 0,
-      gasPrice: 0,
-      provider: new HDWalletProvider(fs.readFileSync('c:\\Users\\sukute\\source\\repos\\Web3JsPractice\\code\\hack.env', 'utf-8'), "https://rfmanufacturer.blockchain.azure.com:3200/cbkb8TY2i3TIO-NyHMiB-k6l"),
-      consortium_id: 1571810881489
+      //type: "quorum"
     },
     abs_traiconsortium_hclmanufacturer_hclmanufacturer: {
       network_id: "*",
